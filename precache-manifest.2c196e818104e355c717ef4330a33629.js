58b7e98a2dca27f566ccd8983de91b16c9005017
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-09-08 21:39:09 +0200

    Updates

---
+++ precache-manifest.2c196e818104e355c717ef4330a33629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6215df697b0d8c34e3e755c771a510",
+    "url": "/scraper-node/index.html"
+  },
+  {
+    "revision": "d3f99f9863d0c0b0168b",
+    "url": "/scraper-node/static/css/main.289f8d23.chunk.css"
+  },
+  {
+    "revision": "6b54747c5daa8c69ba48",
+    "url": "/scraper-node/static/js/2.94924ce6.chunk.js"
+  },
+  {
+    "revision": "d3f99f9863d0c0b0168b",
+    "url": "/scraper-node/static/js/main.d2cb4886.chunk.js"
+  },
+  {
+    "revision": "5ff108ac5ff37bf8c92f",
+    "url": "/scraper-node/static/js/runtime~main.2d502146.js"
+  }
+]);
